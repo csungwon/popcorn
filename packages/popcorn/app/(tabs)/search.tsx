fd51e76fd807e43360f5f76f5e2db86cd6429fcb
Sungwon Cho <csungwon@berkeley.edu>
2025-05-11 22:29:16 +0900

    Get current location

--- packages/popcorn/app/(tabs)/search.tsx
+++ packages/popcorn/app/(tabs)/search.tsx
@@ -1,10 +1,33 @@
+import { useEffect, useState } from 'react'
 import { StyleSheet, View } from 'react-native'
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps'
+import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps'
+import {
+  getCurrentPositionAsync,
+  requestForegroundPermissionsAsync
+} from 'expo-location'
 
 export default function SearchScreen() {
+  const [region, setRegion] = useState<Region>()
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied')
+        return
+      }
+      const location = await getCurrentPositionAsync({})
+      setRegion({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 2 / 69,
+        longitudeDelta: 2 / (69 * Math.cos(location.coords.latitude))
+      })
+    }
+    getCurrentLocation()
+  }, [])
   return (
     <View style={styles.container}>
-      <MapView provider={PROVIDER_GOOGLE} style={styles.map} />
+      <MapView region={region} provider={PROVIDER_GOOGLE} style={styles.map} />
     </View>
   )
 }
