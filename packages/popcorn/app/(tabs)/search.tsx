4252862b72eb6e7ad0169820533e1d226a3cf4ff
Sungwon Cho <csungwon@berkeley.edu>
2025-05-14 17:53:26 +0900

    Fix map style

--- packages/popcorn/app/(tabs)/search.tsx
+++ packages/popcorn/app/(tabs)/search.tsx
@@ -1,43 +1,54 @@
-import { useEffect, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
-import MapView, { PROVIDER_GOOGLE, Region } from 'react-native-maps'
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import {
   getCurrentPositionAsync,
   requestForegroundPermissionsAsync
 } from 'expo-location'
+import { useEffect, useState } from 'react'
+import { View, StyleSheet } from 'react-native'
+import MapView, { PROVIDER_GOOGLE, type Region } from 'react-native-maps'
 
 export default function SearchScreen() {
   const [region, setRegion] = useState<Region>()
+  const tabBarHeight = useBottomTabBarHeight()
+
   useEffect(() => {
     async function getCurrentLocation() {
       let { status } = await requestForegroundPermissionsAsync()
+      await new Promise((resolve) => setTimeout(resolve, 1000))
       if (status !== 'granted') {
-        console.log('Permission to access location was denied')
+        console.error('Permission to access location was denied')
         return
       }
       const location = await getCurrentPositionAsync({})
       setRegion({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-        latitudeDelta: 2 / 69,
-        longitudeDelta: 2 / (69 * Math.cos(location.coords.latitude))
+        latitudeDelta: 2 / 69, // simple calculation for 2 miles
+        longitudeDelta: 2 / (69 * Math.cos(location.coords.latitude)) // simple calculation for 2 miles
       })
     }
     getCurrentLocation()
   }, [])
+
   return (
-    <View style={styles.container}>
-      <MapView region={region} provider={PROVIDER_GOOGLE} style={styles.map} />
+    <View style={[styles.container]}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={[styles.map, { bottom: tabBarHeight }]}
+        showsUserLocation
+        mapType="standard"
+        showsMyLocationButton
+        initialRegion={region}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    ...StyleSheet.absoluteFillObject
   },
   map: {
-    width: '100%',
-    height: '100%'
+    ...StyleSheet.absoluteFillObject
   }
 })
--- packages/popcorn/app/_layout.tsx
+++ packages/popcorn/app/_layout.tsx
@@ -1,30 +1,37 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider
+} from '@react-navigation/native'
+import { useFonts } from 'expo-font'
+import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from '@/hooks/useColorScheme'
 import 'expo-dev-client'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme()
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
+  })
 
   if (!loaded) {
     // Async font loading only occurs in development.
-    return null;
+    return null
   }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <SafeAreaProvider>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+      </SafeAreaProvider>
       <StatusBar style="auto" />
     </ThemeProvider>
-  );
+  )
 }
